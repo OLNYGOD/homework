7bf27e4b93cebae931c99e205ef427f38edea77c
OLNYGOD <abcde5676151199@gmail.com>
2021-04-07 23:44:02 +0800

    Homework first change

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,12 +4,23 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Please enter number of tickets :");
-        Scanner scanner = new Scanner(System.in);
-        int Total_tickets = scanner.nextInt();
-        System.out.println("How many round-trip tickets :");
-        int Round_trip = scanner.nextInt();
-        Ticket ticket = new Ticket(Total_tickets, Round_trip);
-        ticket.Print();
+        int Total_tickets = 0;
+        while (Total_tickets != -1){
+            System.out.println("Please enter number of tickets or Press -1 if you want to stop :");
+            Scanner scanner = new Scanner(System.in);
+            Total_tickets = scanner.nextInt();
+            if (Total_tickets == -1){
+                System.out.print("Stop the system");
+                break;
+            }
+            System.out.println("How many round-trip tickets :");
+            int Round_trip = scanner.nextInt();
+            if (Round_trip > Total_tickets){
+                System.out.println("Round-trip tickets must smaller than tickets number.");
+            }else {
+                Ticket ticket = new Ticket(Total_tickets, Round_trip);
+                ticket.Print();
+            }
+        }
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -4,16 +4,26 @@ import java.util.*
 
 fun main(){
     val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets: ")
-    var ticket = scanner.nextInt()
-    print("How many round-trip tickets: ")
-    var round = scanner.nextInt()
-    val total = TesterKotlin(ticket, round)
-    total.print()
-
+    var ticket = 0
+    while (ticket != -1) {
+        print("Please enter number of tickets or Press -1 if you want to stop : ")
+        ticket = scanner.nextInt()
+        if (ticket == -1){
+            print("Stop the system")
+            break
+        }
+        print("How many round-trip tickets: ")
+        var round = scanner.nextInt()
+        if (ticket < round){
+            println("Round-trip tickets must smaller than tickets number.")
+        }else{
+        val total = TesterKotlin(ticket, round)
+        total.print()
+        }
+    }
 }
 class TesterKotlin(var ticket : Int, var round : Int) {
     fun print(){
-        print("Total tickets:" + ticket + "\t" + "Round-trip:" + round + "\t" + "Total:" + ((ticket - round) * 1000 + round * 2000 * 0.9))
+        println("Total tickets:" + ticket + "\t" + "Round-trip:" + round + "\t" + "Total:" + ((ticket - round) * 1000 + round * 2000 * 0.9))
     }
 }
